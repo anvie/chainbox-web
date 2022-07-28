80f91290c8a29ba76cf22f23abdd42902f668844
robin <robin@ansvia.com>
2022-07-29 01:22:37 +0700

    update section2

--- components/Section2.tsx
+++ components/Section2.tsx
@@ -8,15 +8,15 @@ const Section1 = () => {
       <div
         className={`${styles.overview} justify-center items-left flex flex-col`}
       >
-        <div className="block font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-orange-300 to-pink-600 text-lg">
+        <div className="block font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-orange-300 to-pink-600 text-xl">
           Chainbox
         </div>
 
-        <p className={`${styles.description} max-w-xl`}>
-        Simplify the process of creating Web3 project.
+        <p className={`${styles.description} max-w-xl pt-10`}>
+        Build Web3 project in no time.
         </p>
         <p className={`${styles.description} max-w-xl pt-2`}>
-        From generating smart contract, deploying into blockchain,
+        Auto generated smart contract, deploy into several blockchains,
         and integrate with the web frontend.
         </p>
         <p className={`${styles.description} max-w-xl pt-2`}>
